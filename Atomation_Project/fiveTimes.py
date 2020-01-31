0798cb24fbda118902e13580204f43dc146e8d0f
Veeresh PS <35962662+veeresh-ps@users.noreply.github.com>
2020-01-31 15:15:22 +0530

    Added few more files

---
+++ Atomation_Project/fiveTimes.py
@@ -0,0 +1,10 @@
+print("My name is using for loop")
+
+for i in range(5):
+    print("Veeresh Five Times (" +str(i)+")")
+
+print("My name using while loop")
+i=0
+while i<5:
+   print("Veeresh File TImes("+str(i)+")")
+   i=i+1
