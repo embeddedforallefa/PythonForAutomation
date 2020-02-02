acfc4bf6d66caa462474641f8ee696e8e1b56df2
veeresh-ps <veeru.01jnvu@gmail.com>
2020-02-02 22:34:18 +0530

    Added some more files

---
+++ Atomation_Project/Variable_Scope.py
@@ -0,0 +1,16 @@
+def spam():
+    eggs = 99
+    bacon()
+    print(eggs)
+
+def bacon():
+    ham = 101
+    eggs = 0
+
+def apple():
+    print(apples)
+apples = 42
+
+spam()
+apple()
+print(apples)
